77d6f3e5227150fd98be58fa3bd8b0d8ff66652e
Zachariux <zachagan@rocketmail.com>
2018-04-09 15:54:04 +0930

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment+25);
                 totalChargeLabelTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25+5);
+                GST.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5)*1.1m);
             }
             catch (FormatException)
             {
